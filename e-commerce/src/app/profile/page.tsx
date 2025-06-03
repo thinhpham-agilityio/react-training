1ee178bf17830645030d7b2c98d6e4b63245af07
Thinh Van Phu Pham <thinh.pham@asnet.com.vn>
2025-06-03 16:34:14 +0700

    feat: implement login page #34

---
+++ e-commerce/src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import ProfileSection from "@/components/profile/profile-section";
+
+export default function ProfilePage() {
+  return (
+    <div className="py-4">
+      <div className="container m-auto px-3">
+        <h1 className="text-2xl font-bold mb-6">Profile</h1>
+        <ProfileSection />
+      </div>
+    </div>
+  );
+}
--- e-commerce/src/app/provider.tsx
+++ e-commerce/src/app/provider.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { CartProvider } from '@/hooks/useCartContext';
+import { SessionProvider } from "next-auth/react"
 import { ReactNode } from 'react';
 
 export default function AppProvider({ children }: { children: ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
+  return (
+    <SessionProvider>
+      <CartProvider>{children}</CartProvider>
+    </SessionProvider>
+  );
 }
