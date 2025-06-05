2873a88cf25b4cbaaa29228f872121ac6bb91c5e
thinh.pham <thinh.pham@asnet.com.vn>
2025-06-05 09:21:15 +0700

    fix: update cart icon

--- e-commerce/src/components/header/right-nav.tsx
+++ e-commerce/src/components/header/right-nav.tsx
@@ -1,13 +1,10 @@
-import Link from 'next/link';
 import ShoppingCartIcon from '../icons/shopping-cart-icon';
 import ProfileIcon from '../icons/profile-icon';
 
 const RightNav = () => {
   return (
     <div className="flex items-center gap-6">
-      <Link href="/cart" passHref>
-        <ShoppingCartIcon />
-      </Link>
+      <ShoppingCartIcon />
       <ProfileIcon />
     </div>
   );
--- e-commerce/src/components/icons/shopping-cart-icon.tsx
+++ e-commerce/src/components/icons/shopping-cart-icon.tsx
@@ -2,6 +2,7 @@
 
 import useCartContext from '@/hooks/use-cart-context';
 import { ShoppingCart } from 'lucide-react';
+import Link from 'next/link';
 
 const ShoppingCartIcon = () => {
   const { cart } = useCartContext();
@@ -12,14 +13,16 @@ const ShoppingCartIcon = () => {
   );
 
   return (
-    <div className="relative">
-      <ShoppingCart />
-      {itemCount > 0 && (
-        <div className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-xs text-contained">
-          <span>{`${itemCount}`}</span>
-        </div>
-      )}
-    </div>
+    <Link href="/cart" passHref>
+      <div className="relative">
+        <ShoppingCart />
+        {itemCount > 0 && (
+          <div className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-destructive text-xs text-contained">
+            <span>{`${itemCount}`}</span>
+          </div>
+        )}
+      </div>
+    </Link>
   );
 };
 
