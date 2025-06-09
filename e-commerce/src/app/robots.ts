9e9ca877e8b302551a5d46b54086e4aef482fdfc
thinh.pham <thinh.pham@asnet.com.vn>
2025-06-09 14:10:04 +0700

    fix: fix shift layout when clicking select input

--- e-commerce/src/app/robots.ts
+++ e-commerce/src/app/robots.ts
@@ -4,8 +4,8 @@ import { MetadataRoute } from 'next';
 export const robots = (): MetadataRoute.Robots => ({
   rules: {
     userAgent: '*',
-    allow: [ROUTES.LOGIN, ROUTES.HOME],
-    disallow: [ROUTES.SHOP, ROUTES.CART, ROUTES.PROFILE]
+    allow: [ROUTES.LOGIN, ROUTES.HOME, ROUTES.SHOP],
+    disallow: [ROUTES.CART, ROUTES.PROFILE]
   },
   sitemap: `${process.env.NEXT_PUBLIC_API_URL}/sitemap.xml`
 });
