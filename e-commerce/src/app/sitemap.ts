4746433ccb32d43a5398dfd982edbac67f2fd572
thinh.pham <thinh.pham@asnet.com.vn>
2025-06-11 15:46:02 +0700

    fix: update site map file

--- e-commerce/src/app/sitemap.ts
+++ e-commerce/src/app/sitemap.ts
@@ -4,13 +4,13 @@ import { ROUTES } from '@/constants/routes';
 export const sitemap = (): MetadataRoute.Sitemap => {
   return [
     {
-      url: `${process.env.BASE_URL}/${ROUTES.HOME}`,
+      url: `${process.env.BASE_URL}${ROUTES.HOME}`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
       priority: 1
     },
     {
-      url: `${process.env.BASE_URL}/${ROUTES.LOGIN}`,
+      url: `${process.env.BASE_URL}${ROUTES.LOGIN}`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
       priority: 0.8
