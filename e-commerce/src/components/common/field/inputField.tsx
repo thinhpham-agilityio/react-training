7e58cd243d76039204dcfcd033307de68a907b1f
thinh.pham <thinh.pham@asnet.com.vn>
2025-06-11 17:02:02 +0700

    fix: refactor input component

--- e-commerce/src/components/common/field/inputField.tsx
+++ e-commerce/src/components/common/field/inputField.tsx
@@ -1,63 +1,62 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import { cn } from '@/lib/utils';
 import { LucideProps } from 'lucide-react';
-import { cloneElement, ReactElement } from 'react';
+import { ComponentType, forwardRef, ReactNode } from 'react';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
-  startIcon?: React.ReactNode;
-  endIcon?: React.ReactNode;
+  startIcon?: ReactNode | ComponentType<LucideProps>;
+  endIcon?: ReactNode | ComponentType<LucideProps>;
   iconProps?: LucideProps;
-  ref?: React.Ref<HTMLInputElement>;
 }
 
-const InputField = ({
-  className,
-  type,
-  startIcon,
-  endIcon,
-  iconProps = {},
-  ref,
-  ...props
-}: InputProps) => {
-  const { className: iconClassName, ...iconRest } = iconProps;
+const InputField = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, startIcon, endIcon, iconProps = {}, ...props }, ref) => {
+    const { className: iconClassName, ...iconRest } = iconProps;
 
-  return (
-    <div className="relative">
-      {startIcon && (
-        <div className="absolute top-1/2 left-4 -translate-y-1/2 transform">
-          {cloneElement(startIcon as ReactElement<any>, {
-            size: 24,
-            className: cn('text-muted-foreground', iconClassName),
-            ...iconRest
-          })}
-        </div>
-      )}
-      <input
-        type={type}
-        className={cn(
-          'border-input bg-background ring-offset-background placeholder:text-secondary/40 focus-visible:ring-secondary flex min-h-10 w-full rounded-full border px-8 py-3 text-sm text-zinc-900 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 lg:text-base',
-          startIcon ? 'pl-12' : '',
-          endIcon ? 'pr-12' : '',
-          className
+    const renderIcon = (icon: ReactNode | ComponentType<LucideProps>) => {
+      if (typeof icon === 'function') {
+        const IconComponent = icon as ComponentType<LucideProps>;
+
+        return (
+          <IconComponent
+            size={24}
+            className={cn('text-muted-foreground', iconClassName)}
+            {...iconRest}
+          />
+        );
+      }
+      return icon;
+    };
+
+    return (
+      <div className="relative">
+        {startIcon && (
+          <div className="absolute top-1/2 left-4 -translate-y-1/2 transform">
+            {renderIcon(startIcon)}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
-      {endIcon && (
-        <div className="absolute top-1/2 right-3 -translate-y-1/2 transform">
-          {cloneElement(endIcon as ReactElement<any>, {
-            size: 24,
-            className: cn('text-muted-foreground', iconClassName),
-            ...iconRest
-          })}
-        </div>
-      )}
-    </div>
-  );
-};
+        <input
+          type={type}
+          className={cn(
+            'border-input bg-background ring-offset-background placeholder:text-secondary/40 focus-visible:ring-secondary flex min-h-10 w-full rounded-full border px-8 py-3 text-sm text-zinc-900 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 lg:text-base',
+            startIcon ? 'pl-12' : '',
+            endIcon ? 'pr-12' : '',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {endIcon && (
+          <div className="absolute top-1/2 right-3 -translate-y-1/2 transform">
+            {renderIcon(endIcon)}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 InputField.displayName = 'Input';
 
